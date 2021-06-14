e2ac797392f82ab43dbb9332cf13034b5180586c
Jacqui.Theunissen <Jacqui.Theunissen@KPNBJACQUILOAN>
2021-06-14 17:46:34 +0200

    Add project files.

---
+++ CoffeApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new OrderService();
+            order.ProcessOrder();
+        }
+    }
+}
